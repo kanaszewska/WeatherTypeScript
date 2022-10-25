b55f479447e8ab1426a84542acd65c57214f22e3
Dominika Dębska <kanaszewskadominika@gmail.com>
2022-10-25 19:53:45 +0200

    first commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,89 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import axios from 'axios';
 import './App.css';
 
+interface MainData {
+  temp: number,
+  weather: string,
+  feels_like: string,
+  humidity: number,
+};
+
+interface WindData {
+  speed: number;
+};
+
+interface WeatherData {
+  name: string,
+  main: MainData | null,
+  wind: WindData | null,
+};
+
 function App() {
+  const [data, setData] = useState<WeatherData>({
+    name: '',
+    main: null,
+    wind: null,
+  });
+
+  const [location, setLocation] = useState<string>('');
+
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&lang=en&units=metric&appid=cbb432a3507e58ebea05da61a8461ca1`;
+
+  const useLocation = () => {
+    axios.get<WeatherData>(url)
+    .then((response) => {
+      setData(response.data)
+      console.log(response.data)
+    })
+    setLocation('')
+};
+
+const handleOnChange = (e: any): void => {
+  setLocation(e.target.value);
+};
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='app'>
+      <div className='search'>
+        <input 
+          value={location}
+          onChange={handleOnChange}
+          placeholder='Enter a location...'
+          type="text" 
+        />
+        <button
+          onClick={useLocation}
+        >Search
+        </button>
+      </div>
+      <div className="container">
+          <div className="top">
+            <div className="location">
+              <p>{data.name}</p>
+            </div>
+            <div className="temp">
+              {data.main ? <h1>{data.main.temp}°C</h1> : null}
+            </div>
+            <div className="description">
+              {data.main ? <p>{data.main.weather}</p> : null}
+            </div>
+          </div>
+          <div className="bottom">
+            <div className="feels">
+              {data.main ? <p>{data.main.feels_like}°C</p> : null}
+              <h4>Sensed temperature</h4>
+            </div>
+            <div className="humidity">
+              {data.main ? <p>{data.main.humidity}%</p> : null}
+              <h4>Humidity</h4>
+            </div>
+            <div className="wind">
+              {data.wind ? <p>{data.wind.speed}MPH</p> : null}
+              <h4>Wind speed</h4>
+            </div>
+          </div>
+      </div>
     </div>
   );
 }
